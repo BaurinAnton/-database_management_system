99b9b28abae7d079ecb6f1fdb8c75641a1e9b58c
BaurinAnton <baurinanton2013@yandex.ru>
2021-11-21 23:49:19 +0300

    Объявлен динамический одномерный массив

--- firstPractical/firstPractical.cpp
+++ firstPractical/firstPractical.cpp
@@ -6,4 +6,13 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "Russian");
+    //Объявление переменной размерности одномерного массива
+    int arraySize;
+    cout << "Введите размер одномерного массива:";
+    //Ввод размерности одномерного массива
+    cin >> arraySize;
+    //Объявление динамического одномерного массива
+    //* ptrarray - адрес первого элемента массива
+    char* ptrarray = new char[arraySize];
+    delete [] ptrarray;
 }
