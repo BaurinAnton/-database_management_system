ed1e31edc37a77eb7a69b5676b6faa560a7b2d3b
BaurinAnton <baurinanton2013@yandex.ru>
2021-11-21 23:50:38 +0300

    Разработана функция заполнения одномерного массива

--- firstPractical/firstPractical.cpp
+++ firstPractical/firstPractical.cpp
@@ -3,6 +3,23 @@
 #include <stdlib.h>
 using namespace std;
 
+//Функция заполнения одномерного массива
+void fillingTheArray(int arraySize, char *ptrarray) {
+    //Функция srand генерирует псевдослучайные числа относительно 
+    //возвращенного значения времени в секундах на данный момент
+    //unsigned - целое положительное число.
+    srand(unsigned(time(NULL)));
+    //Заполнение одномерного массива псевдослучайными числами и вывод одномерного массива
+    cout << endl << "Заполнения массива псевдослучайными числами";
+    cout << endl << "Одномерный массив = ";
+    for (int count = 0; count < arraySize; count++) {
+        //Данный диапазон чисел по таблице ASII - заглавные буквы английского алфавита
+        //Причина данного диапазона - наглядное отображения одномерного массива в консоли
+        ptrarray[count] = 65 + rand() % (90 - 65 + 1);
+        cout << ptrarray[count];
+    }
+}
+
 int main()
 {
     setlocale(LC_ALL, "Russian");
@@ -14,5 +31,7 @@ int main()
     //Объявление динамического одномерного массива
     //* ptrarray - адрес первого элемента массива
     char* ptrarray = new char[arraySize];
+    //Функция заполнения массива
+    fillingTheArray(arraySize, ptrarray);
     delete [] ptrarray;
 }
