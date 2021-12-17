aeb69dd5d4a5a9fec02a826e5849f8c842a528d6
BaurinAnton <baurinanton2013@yandex.ru>
2021-12-17 14:53:21 +0300

    1. Создан пустой проект 2. Создана проект статической библиотеки 3. Создан проект нативных тестов

---
+++ CircularSinglyLinkedListSolution/CircularSinglyLinkedList.Lib/CircularSinglyLinkedList.Lib.cpp
@@ -0,0 +1,10 @@
+﻿// CircularSinglyLinkedList.Lib.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: Это пример библиотечной функции.
+void fnCircularSinglyLinkedListLib()
+{
+}
---
+++ CircularSinglyLinkedListSolution/CircularSinglyLinkedList.Test/CircularSinglyLinkedList.Test.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CircularSinglyLinkedListTest
+{
+	TEST_CLASS(CircularSinglyLinkedListTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
